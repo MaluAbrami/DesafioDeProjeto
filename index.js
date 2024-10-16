0c1fa25a7faa139d361ce8541f17eee6d7a667b5
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-16 20:36:48 -0300

    add encadeamento de if para determinar nivel do heroi

--- index.js
+++ index.js
@@ -56,5 +56,22 @@ while(vidaDoHeroi > 0){
 
         }
     }
-    
+
+    if(experienciaHeroi <= 1000)
+        nivelHeroi = "Ferro";
+    else if(experienciaHeroi <= 2000)
+        nivelHeroi = "Bronze";
+    else if(experienciaHeroi <= 5000)
+        nivelHeroi = "Prata";
+    else if(experienciaHeroi <= 7000)
+        nivelHeroi = "Ouro";
+    else if(experienciaHeroi <= 8000)
+        nivelHeroi = "Platina";
+    else if(experienciaHeroi <= 9000)
+        nivelHeroi = "Ascendente";
+    else if(experienciaHeroi <= 10000)
+        nivelHeroi = "Imortal";
+    else
+        nivelHeroi = "Radiante";
+
 }

b65ce1fe095bba3c775dc3d9920758c818be838a
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-16 20:27:53 -0300

    add mensagem introdutória para o usuário

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ let dado;
 let vidaDoHeroi = 10;
 //Declarei a variável 'rodada' para armazenar a quantidade de rodadas totais que ocorrerem durante o 'while' e acompanhar a rodada atual a cada repetição
 let rodada = 0;
+
+console.log(`
+INTRODUÇÃO AO GAME:
+\nAqui você testará a sua sorte e até que nível de herói ela permitirá que você chegue.
+A cada rodada um dado de 6 lados será lançado e dependendo do seu nível atual o número que cair pode ou não te ajudar.
+Se o número que cair for o que o seu nível de herói pede, então você ganhará a quantia de XP determinada pelo nível.
+Porém, se o número que cair não for o que se espera no seu nível atual, você ganhará metade do XP padrão do seu nível e perderá 1 vida.
+No fim, você saberá quantas rodadas seu herói sobreviveu, qual foi o nível final que ele atingiu e o total de XP ganho.
+Te desejo boa sorte!
+`);

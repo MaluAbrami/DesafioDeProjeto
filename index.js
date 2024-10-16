743fe76876c37d554b035ec97894c5d7690be907
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-16 20:27:02 -0300

    add variáveis

---
+++ index.js
@@ -0,0 +1,12 @@
+//Declarei a constante 'nomeHeroi' para armazenar o nome do meu herói que nesse caso chamei de Killjoy
+const nomeHeroi = "Killjoy"; 
+//Declarei as variáveis necessárias para o Desafio e atribui valores iniciais minímos
+let experienciaHeroi = 0;
+let nivelHeroi = "Ferro";
+
+// Declarei a variável 'dado' para poder armazenar o número aleatório entre 1 e 6 que simule um dado de 6 lados real
+let dado;
+//Declarei a variável 'vidaDoHeroi' para por tornar o "joguinho" mais dinâmico e também para utilizá-lo como condição de parada na minha estrutura de repetição 'while'
+let vidaDoHeroi = 10;
+//Declarei a variável 'rodada' para armazenar a quantidade de rodadas totais que ocorrerem durante o 'while' e acompanhar a rodada atual a cada repetição
+let rodada = 0;

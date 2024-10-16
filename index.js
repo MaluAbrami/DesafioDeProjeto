4df315aaaca2dab921b8816f950e3bb1cfa2b934
MaluAbrami <marialuizaabrami@gmail.com>
2024-10-16 20:46:01 -0300

    add comentários ao código

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let vidaDoHeroi = 10;
 //Declarei a variável 'rodada' para armazenar a quantidade de rodadas totais que ocorrerem durante o 'while' e acompanhar a rodada atual a cada repetição
 let rodada = 0;
 
+//Aqui utilizei o console.log() para exibir uma mensagem introdutória ao usuário, para explicar como funciona a dinâmica do "jogo"
 console.log(`
 INTRODUÇÃO AO GAME:
 \nAqui você testará a sua sorte e até que nível de herói ela permitirá que você chegue.
@@ -21,18 +22,26 @@ No fim, você saberá quantas rodadas seu herói sobreviveu, qual foi o nível f
 Te desejo boa sorte!
 `);
 
+//Utilizei um 'while' para garantir que a distribuição de XP para o herói só encerrasse quando suas 10 vidas iniciais acabassem
 while(vidaDoHeroi > 0){
 
-    if(nivelHeroi != "Radiante"){
-        rodada++;
+    if(nivelHeroi != "Radiante"){ //Se o herói já está no último nível, então não é necessário rolar o dado mais uma vez e nem atualizar a rodada 
+        rodada++; //atualizando a rodada atual
+
         console.log("\nVocê está na rodada " + rodada + " e no nível " + nivelHeroi + ". No momento, " + nomeHeroi + " está com " + vidaDoHeroi + " de vida e com " + experienciaHeroi + " de XP.");
+        
+        //Utilizei o Math.random() para gerar um número aleatório entre 1 e 6, por isso multipliquei ele po 6 e acrescentei 1, assim determino que seu inicio é em 1 e vai até 6
+        //Utilizei o Math.floor() para garantir que o número gerado não seja um decimal(Já que o random gera números decimais), arredondando o número para baixo
         dado = Math.floor(Math.random() * 6) + 1;
         console.log("O valor que caiu no dado foi " + dado);
     }
 
+    //Utilizei um 'switch' para garantir uma distribuição de XP diferente para cada nível do herói
+    //Cada nível pode solicitar números do dado específicos e diferentes dos outros níveis
+    //Além disso, a cada nível do herói as chances de conseguir tirar o número no dado diminuem
     switch(nivelHeroi){
         case "Ferro":{
-            experienciaHeroi += (dado <= 5) ? 200 : 100; 
+            experienciaHeroi += (dado <= 5) ? 200 : 100; //Utilizei funções lambdas para deixar meu código mais conciso e limpo
             (dado <= 5) ? console.log("+200XP") : (console.log("+100XP\t-1 vida"), vidaDoHeroi--); 
             break;
         }
@@ -68,11 +77,12 @@ while(vidaDoHeroi > 0){
         }
         default:{
             console.log("Parabéns! Você chegou ao último nível! Então agora é um Radiante!");
-            vidaDoHeroi = 0;
+            vidaDoHeroi = 0; //Aqui eu zero a vida do heroi(caso ainda reste alguma) para encerrar o nosso 'while'
             break;
         }
     }
 
+    //Utilizei vários if encadeados para verificar qual o nível atual do herói de acordo com seu total de XP
     if(experienciaHeroi <= 1000)
         nivelHeroi = "Ferro";
     else if(experienciaHeroi <= 2000)
@@ -92,4 +102,5 @@ while(vidaDoHeroi > 0){
 
 }
 
+//Feedback para o usuário saber quantas rodadas foram, nível final do seu herói e quanto XP ele fez no total
 console.log("\n\nApós " + rodada + " rodadas, o nível final de " + nomeHeroi + " é " + nivelHeroi + " com " + experienciaHeroi + " de XP.");
